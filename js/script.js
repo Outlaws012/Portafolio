39d231800b2ae28599b62a193fd21650f8c1acf1
Outlaws012 <outlaws1012@gmail.com>
2023-04-08 12:00:38 -0500

    Pagina Terminada

--- js/script.js
+++ js/script.js
@@ -62,10 +62,13 @@ darkModeIcon.onclick = () => {
 };
 
 ScrollReveal({  
-   reset: true,
+    reset: true,
    distance: '80px',
    duration: 2000,
    delay: 200
   });
 
   ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+  ScrollReveal().reveal('.home-img img, .services-container, .portafolio-box, .testimonial-wrapper, .contact form', { origin: 'bottom' });
+  ScrollReveal().reveal('.home-content h1, .about-img img', { origin: 'left' });
+  ScrollReveal().reveal('.home-content h3, .home-content p, .about-content', { origin: 'right' });
